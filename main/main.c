36a717a8b7d4804acbf8f53a07f376ec91d2eff1
ves <viorel_serbu@yahoo.com>
2024-09-19 10:41:03 +0300

    v2.1

--- main/main.c
+++ main/main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_netif.h"
 #include "esp_event.h"
@@ -23,7 +24,7 @@
 #include "esp_console.h"
 #include "driver/gpio.h"
 #include "errno.h"
-#include "esp_spi_flash.h"
+#include "spi_flash_mmap.h"
 #include "esp_spiffs.h"
 #include "esp_vfs_dev.h"
 #include "esp_vfs_fat.h"
@@ -38,30 +39,16 @@
 #include "cmd_wifi.h"
 #include "tcp_log.h"
 
-#if CONFIG_EXAMPLE_CONNECT_WIFI
-#include "esp_wifi.h"
-#endif
-
-//int rw_params(int rw, int param_type, void * param_val);
-
 #define TAG "OTA_FACT"
 #define PROMPT_STR "OTA_FACT"
-#define CONFIG_STORE_HISTORY 0
+#define CONFIG_STORE_HISTORY 1
 #define CONFIG_CONSOLE_MAX_COMMAND_LINE_LENGTH	1024
 
 #include "wifi_credentials.h"
 
-int console_state;
+console_state_t console_state;
 int restart_in_progress;
 int controller_op_registered;
-
-esp_vfs_spiffs_conf_t conf_spiffs =
-	{
-	.base_path = BASE_PATH,
-	.partition_label = PARTITION_LABEL,
-	.max_files = 5,
-	.format_if_mount_failed = true
-	};
 
 
 static void initialize_nvs(void)
@@ -75,53 +62,23 @@ static void initialize_nvs(void)
 	ESP_ERROR_CHECK(err);
 	}
 
-#if CONFIG_STORE_HISTORY
-	#define MOUNT_PATH "/data"
-	#define HISTORY_PATH MOUNT_PATH "/history.txt"
-	static void initialize_filesystem(void)
-		{
-		static wl_handle_t wl_handle;
-		const esp_vfs_fat_mount_config_t mount_config =
-			{
-			.max_files = 4,
-			.format_if_mount_failed = true
-			};
-		esp_err_t err = esp_vfs_fat_spiflash_mount(MOUNT_PATH, "storage", &mount_config, &wl_handle);
-		if (err != ESP_OK)
-			{
-			ESP_LOGE(TAG, "Failed to mount FATFS (%d/%s)", err, esp_err_to_name(err));
-			return;
-			}
-		}
-#endif // CONFIG_STORE_HISTORY
-
-
 void app_main(void)
 	{
 	console_state = CONSOLE_OFF;
 	setenv("TZ","EET-2EEST,M3.4.0/03,M10.4.0/04",1);
-	esp_err_t ret = esp_vfs_spiffs_register(&conf_spiffs);
-	if (ret != ESP_OK)
-		{
-        if (ret == ESP_FAIL)
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        else if (ret == ESP_ERR_NOT_FOUND)
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-        else
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-        esp_restart();
-		}
+	spiffs_storage_check();
 	initialize_nvs();
 	controller_op_registered = 0;
-	rw_params(PARAM_READ, PARAM_CONSOLE, &console_state);
+	tsync = 0;
 	wifi_join(DEFAULT_SSID, DEFAULT_PASS, JOIN_TIMEOUT_MS);
+	rw_params(PARAM_READ, PARAM_CONSOLE, &console_state);
+	tcp_log_evt_queue = NULL;
 	tcp_log_init();
 	esp_log_set_vprintf(my_log_vprintf);
+	sync_NTP_time();
 
-	xTaskCreate(ntp_sync, "NTP_sync_task", 6134, NULL, USER_TASK_PRIORITY, &ntp_sync_task_handle);
-	register_system();
-	if(mqtt_start() == ESP_OK)
-		register_mqtt();
+	if(mqtt_start() != ESP_OK)
+		esp_restart();
 #ifdef WITH_CONSOLE
 	esp_console_repl_t *repl = NULL;
     esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
@@ -133,10 +90,11 @@ void app_main(void)
 
 
 #if CONFIG_STORE_HISTORY
-	initialize_filesystem();
-	repl_config.history_save_path = HISTORY_PATH;
+	//initialize_filesystem();
+	repl_config.history_save_path = BASE_PATH HISTORY_FILE;
 	ESP_LOGI(TAG, "Command history enabled");
 #else
+	repl_config.history_save_path = NULL;
 	ESP_LOGI(TAG, "Command history disabled");
 #endif
 #endif
@@ -149,6 +107,7 @@ void app_main(void)
 #ifdef WITH_CONSOLE
 #if defined(CONFIG_ESP_CONSOLE_UART_DEFAULT) || defined(CONFIG_ESP_CONSOLE_UART_CUSTOM)
     esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    repl_config.task_stack_size = 8192;
     ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
 
 #elif defined(CONFIG_ESP_CONSOLE_USB_CDC)
